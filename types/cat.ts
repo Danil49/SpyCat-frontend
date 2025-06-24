6546af4369eac84fb6eee0b31ce893afca74c1eb
Danil49 <58887592+Danil49@users.noreply.github.com>
2025-06-24 16:10:20 +0200

    feat: init cat type

---
+++ types/cat.ts
@@ -0,0 +1,20 @@
+export interface Cat {
+  id: number
+  name: string
+  years_of_experience: number
+  breed: string
+  salary: number
+  created_at: string
+  updated_at?: string
+}
+
+export interface CatCreate {
+  name: string
+  years_of_experience: number
+  breed: string
+  salary: number
+}
+
+export interface CatUpdate {
+  salary: number
+}
